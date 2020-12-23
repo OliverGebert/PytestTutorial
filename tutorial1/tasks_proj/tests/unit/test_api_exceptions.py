fe2b85889fb2287a4b6b37be9047a9a1b52fceda
Oli G <oli@Macintosh.fritz.box>
2020-12-23 19:09:30 +0100

    exception db raise

--- tutorial1/tasks_proj/tests/unit/test_api_exceptions.py
+++ tutorial1/tasks_proj/tests/unit/test_api_exceptions.py
@@ -4,4 +4,11 @@ import tasks
 def test_add_raises():
     # add() should raise an exception with wrong type param
     with pytest.raises(TypeError):
-        tasks.add(tasks='not a Task object')
+        tasks.add(task = 'not a Task object')
+
+def test_start_tasks_db_raises():
+    # make sure unsuported db raises an exception
+    with pytest.raises(ValueError) as excinfo:
+        tasks.start_tasks_db('some/great/path', 'mysql')
+    exception_msg = excinfo.value.args[0]
+    assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
